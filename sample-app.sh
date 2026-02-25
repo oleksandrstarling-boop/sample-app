b0570b0401b94fbb8b299808c2e9767d8639476d
Oleksandr Shpakovskyi <oleksandr.starling@gmail.com>
2026-02-25 22:04:31 +0000

    Changed port from 8080 to 5050.

---
+++ sample-app.sh
@@ -0,0 +1,18 @@
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+cp sample_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+echo "FROM python:3.11-slim" >> tempdir/Dockerfile
+echo "RUN pip install --no-cache-dir --progress-bar off flask==2.3.3" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
+cd tempdir
+
+docker build -t sampleapp .
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker ps -a
